f2865b073dc70c08e4eb9e6bbb37e9f180a502ba
Anastasia <chestaci@gmail.com>
2023-06-20 14:13:10 +0300

    initial commit

--- src/test/java/com/github/Chestaci/tests/way2automation_test/AuthorizationTest.java
+++ src/test/java/com/github/Chestaci/tests/way2automation_test/AuthorizationTest.java
@@ -158,12 +158,12 @@ public class AuthorizationTest{
     /**
      * Падающий тест для демонстрации прикрепления скриншотов в отчёте при падении теста
      */
-//    @Test
-//    @Severity(value = SeverityLevel.TRIVIAL)
-//    @Feature("Падающий тест")
-//    @Story("Падающий тест для демонстрации прикрепления скриншотов в отчёте")
-//    public void failTestAuthorizationTest(){
-//        loginPage.clearFields();
-//        Assertions.fail("Fail authorization test");
-//    }
+    @Test
+    @Severity(value = SeverityLevel.TRIVIAL)
+    @Feature("Падающий тест")
+    @Story("Падающий тест для демонстрации прикрепления скриншотов в отчёте")
+    public void failTestAuthorizationTest(){
+        loginPage.clearFields();
+        Assertions.fail("Fail authorization test");
+    }
 }
