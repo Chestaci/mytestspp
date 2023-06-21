4bdac0261c7f8ff058c15be846ec32725b75ce86
Anastasia <chestaci@gmail.com>
2023-06-21 17:43:50 +0300

    initial commit

--- src/test/java/com/github/Chestaci/tests/way2automation_test/AuthorizationTest.java
+++ src/test/java/com/github/Chestaci/tests/way2automation_test/AuthorizationTest.java
@@ -62,7 +62,7 @@ public class AuthorizationTest{
     @DataProvider(name = "authorizationData")
     public Object[][] getAuthorizationData() {
         return new Object[][]{
-                {"angula", "password", "12dfRAsE3!@#$%^&*"},
+                {"angular", "password", "12dfRAsE3!@#$%^&*"},
         };
     }
 
