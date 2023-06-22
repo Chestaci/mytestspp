8607b5ec650ccc0ddd8606a9689c999af845d91a
Anastasia <chestaci@gmail.com>
2023-06-22 17:40:45 +0300

    initial commit

--- src/test/java/com/github/Chestaci/tests/way2automation_test/AuthorizationTest.java
+++ src/test/java/com/github/Chestaci/tests/way2automation_test/AuthorizationTest.java
@@ -158,6 +158,7 @@ public class AuthorizationTest{
     /**
      * Падающий тест для демонстрации прикрепления скриншотов в отчёте при падении теста
      */
+
 //    @Test
 //    @Severity(value = SeverityLevel.TRIVIAL)
 //    @Feature("Падающий тест")
